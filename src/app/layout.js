b0f405d4b9622f63de1b38c3a098c7e3129a27a3
TheHamsterDog <shreshth@shreshthverma.me>
2023-07-04 19:29:13 +0530

    feat: fixed og image

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,17 +9,23 @@ export const metadata = {
   title: 'Shreshth Verma',
   "og:title": 'Shreshth Verma',
   description: 'I\'m Shreshth Verma: A Full Stack Developer, a perpetual student, an entrepreneur, and an amateur poet',
-  "og:description": 'I\'m Shreshth Verma: A Full Stack Developer, a perpetual student, an entrepreneur, and an amateur poet',
-  "og:url":"https://www.shreshthverma.me",
+  openGraph:{
+    title:"Shreshth Verma",
+    description:'I\'m Shreshth Verma: A Full Stack Developer, a perpetual student, an entrepreneur, and an amateur poet',
+    url:"https://www.shreshthverma.me",
+    "images":["/profile.jpeg"]
+  },
   "keywords":"Shreshth, Verma, Shreshth Verma, shreshthverma.me, Personal Site, personal, site",
-  "og:image":"/profile.jpeg"
+
   
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    
+      <Head>
+        <meta name='og:image' content='/profile.jpeg'></meta>
+      </Head>
       <body className={inter.className}>{children}    <Footer />
   </body>
 
