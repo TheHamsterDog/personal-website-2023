d7754e27d4888138b8969d2f739e1146dda2d1b6
TheHamsterDog <shreshth@shreshthverma.me>
2023-07-05 19:23:00 +0530

    feat: updated elect.io link

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -10,7 +10,7 @@ const projects = [
     },
     {
         title: "Elect.io",
-        link: "https://electio.shreshthverma.me",
+        link: "https://github.com/Elect-io/elect.io",
         description: "It is a web app that seeks to help the masses vote in a more educated manner by matching their opinions against politicians",
         technologiesUsed: "MongoDB(Mongoose), Node.JS, Express, React, Redux, SCSS, Material UI, JavaScript, MailJet",
         image: "/electio.png"
